8888331fa127ff3d4864d7020d1ff9dd2ebeca31
Deisy Lorena Restrepo Montoya <deisremo@suramericana.com.co>
2020-11-06 13:30:21 -0500

    cambios

--- EntregableER/src/main/java/com/mycompany/entregableer/EntregableER.java
+++ EntregableER/src/main/java/com/mycompany/entregableer/EntregableER.java
@@ -21,19 +21,19 @@ public class EntregableER {
             int opcion;
            
             String menu ="****Expresiones Irregulares****"
-                + "\n1. Placas de carro el Colombia"
+                + "\n1. Placas de carro el Colombia" /*Listo*/
                 + "\n2. Números de teléfonos fijos en Medellín"
                 + "\n3. Números de teléfonos celulares en Colombia"
-                + "\n4. Direcciones residenciales en Medellín"
-                + "\n5. Direcciones de correos electrónicos"
+                + "\n4. Direcciones residenciales en Medellín" /*Listo*/
+                + "\n5. Direcciones de correos electrónicos" /*Listo*/
                 + "\n6. Fecha en formato dd mm aaaa"
                 + "\n7. Cedulas en Colombia"
                 + "\n8. Una declaracion de variables en java"
-                + "\n9. URL"
-                + "\n10. Nombres y apellidos"
-                + "\n11. Contaseña segura";
+                + "\n9. URL"/*Listo*/
+                + "\n10. Nombres y apellidos" /*Listo*/
+                + "\n11. Contaseña segura"; /*Listo*/
             
-            
+            //https://www.regextester.com/97987
         do{
             opcion = Integer.parseInt(JOptionPane.showInputDialog(menu));
             switch(opcion){ 
@@ -44,6 +44,8 @@ public class EntregableER {
               break;
 
             case 2: 
+                cadena = JOptionPane.showInputDialog(null, "Ingrese dato:", TITLE_MENU, JOptionPane.INFORMATION_MESSAGE);    
+                ejercicio.fijos(cadena);                
               break;
 
             case 4: 
@@ -52,11 +54,27 @@ public class EntregableER {
               break;
 
             case 5: 
-                
+                cadena = JOptionPane.showInputDialog(null, "Ingrese dato:", TITLE_MENU, JOptionPane.INFORMATION_MESSAGE);    
+                ejercicio.email(cadena);                   
               break;
 
             case 6: 
               break;
+              
+            case 9: 
+                cadena = JOptionPane.showInputDialog(null, "Ingrese dato:", TITLE_MENU, JOptionPane.INFORMATION_MESSAGE);    
+                ejercicio.url(cadena);                   
+              break;                
+              
+            case 10: 
+                cadena = JOptionPane.showInputDialog(null, "Ingrese dato:", TITLE_MENU, JOptionPane.INFORMATION_MESSAGE);    
+                ejercicio.nombres(cadena);                   
+              break;     
+              
+            case 11: 
+                cadena = JOptionPane.showInputDialog(null, "Ingrese dato:", TITLE_MENU, JOptionPane.INFORMATION_MESSAGE);    
+                ejercicio.contraena(cadena);                   
+              break;                
             }        
 
         } while(opcion != 0);             
--- EntregableER/src/main/java/com/mycompany/entregableer/ejericcios.java
+++ EntregableER/src/main/java/com/mycompany/entregableer/ejericcios.java
@@ -26,12 +26,54 @@ public class ejericcios {
     }
     
     public void fijos(String dato){
-              
+             
+        Pattern patron = Pattern.compile("(\\+34|0034|34)?[ -]*(8|9)[ -]*([0-9][ -]*){8}",Pattern.CASE_INSENSITIVE); //solo binarios
+        Matcher resultado = patron.matcher(dato);
+        if (resultado.matches()) { //valida cadena completa
+                JOptionPane.showMessageDialog(null, "Cumple");
+        }else {
+                JOptionPane.showMessageDialog(null, "no cumple");
+        };          
     }  
+    
+    
+    public void email(String dato){
+             
+        Pattern patron = Pattern.compile("[\\w\\._]{5,30}\\+?[\\w]{0,10}@[\\w\\.\\-]{3,}\\.\\w{2,5}",Pattern.CASE_INSENSITIVE); //solo binarios
+        Matcher resultado = patron.matcher(dato);
+        if (resultado.matches()) { //valida cadena completa
+                JOptionPane.showMessageDialog(null, "Cumple");
+        }else {
+                JOptionPane.showMessageDialog(null, "no cumple");
+        };          
+    }  
+        
+    
+    public void nombres(String dato){
+             
+        Pattern patron = Pattern.compile("^([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\']+[\\s])+([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])+[\\s]?([A-Za-zÁÉÍÓÚñáéíóúÑ]{0}?[A-Za-zÁÉÍÓÚñáéíóúÑ\\'])?$",Pattern.CASE_INSENSITIVE); //solo binarios
+        Matcher resultado = patron.matcher(dato);
+        if (resultado.matches()) { //valida cadena completa
+                JOptionPane.showMessageDialog(null, "Cumple");
+        }else {
+                JOptionPane.showMessageDialog(null, "no cumple");
+        };          
+    }      
     
     public void celular(String dato){
               
     } 
+    
+    public void url(String dato){
+        Pattern patron = Pattern.compile("^https?:\\/\\/[\\w\\-]+(\\.[\\w\\-]+)+[/#?]?.*$",Pattern.CASE_INSENSITIVE); //solo binarios
+                                                                   // \\w?\\s*#\\s*[0-9]+(-)[0-9]+
+        Matcher resultado = patron.matcher(dato);
+        if (resultado.matches()) { //valida cadena completa
+                JOptionPane.showMessageDialog(null, "Cumple");
+        }else {
+                JOptionPane.showMessageDialog(null, "no cumple");
+        };         
+    }      
     
     public void direccion(String dato){
         Pattern patron = Pattern.compile("((calle|carrera|diagonal|transversal)\\s*[0-9]{1,3})\\w?\\s*#\\s*[0-9]{1,3}",Pattern.CASE_INSENSITIVE); //solo binarios
@@ -43,4 +85,20 @@ public class ejericcios {
                 JOptionPane.showMessageDialog(null, "no cumple");
         };         
     }    
+    
+    public void contraena(String dato){
+        
+        /*La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula, al menos una mayúscula y al menos un caracter no alfanumérico.
+        Ejemplo:
+        w3Unpo<code>t0d0*/        
+        
+        Pattern patron = Pattern.compile("^(?=.*\\d)(?=.*[\\u0021-\\u002b\\u003c-\\u0040])(?=.*[A-Z])(?=.*[a-z])\\S{8,16}$",Pattern.CASE_INSENSITIVE);                                                              
+        Matcher resultado = patron.matcher(dato);
+        if (resultado.matches()) { //valida cadena completa
+                JOptionPane.showMessageDialog(null, "Cumple");
+        }else {
+                JOptionPane.showMessageDialog(null, "no cumple");
+        };         
+    }  
+         
 }
